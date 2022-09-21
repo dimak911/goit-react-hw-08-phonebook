edd5c56149b00c8267923fe83745089cffdf3d3a
dimak911 <dimak911@gmail.com>
2022-09-21 18:48:00 +0000

    Deploying to gh-pages from @ dimak911/goit-react-hw-08-phonebook@9a5d3b8b6c0cac22ba9d5d531a63bbadcc7fff52 🚀

---
+++ static/js/186.371ab3c4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{3186:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Registration"})})})}}}]);
+//# sourceMappingURL=186.371ab3c4.chunk.js.map
---
+++ static/js/206.70837d32.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Welcome!"})}}}]);
+//# sourceMappingURL=206.70837d32.chunk.js.map
